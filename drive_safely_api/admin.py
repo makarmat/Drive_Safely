d72c31ec26590e874b66e0c7327c78849582ae37
Mateusz Makarewicz <mateusz.makarewicz@interia.pl>
2019-11-26 20:08:27 +0100

    Add Photo model

--- drive_safely_api/admin.py
+++ drive_safely_api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from drive_safely_api.models import Advice
+from drive_safely_api.models import Advice, Photo
 
 admin.site.register(Advice)
+admin.site.register(Photo)
--- drive_safely_api/models.py
+++ drive_safely_api/models.py
@@ -3,11 +3,25 @@ from taggit.managers import TaggableManager
 
 # Create your models here.
 
+def images_upload_path(instance, filename):
+    return '{}'.format(filename)
+
+    
+class Photo(models.Model):
+    image = models.ImageField(upload_to = images_upload_path, width_field='width', height_field='height')
+    width = models.IntegerField(editable=False)
+    height = models.IntegerField(editable=False)
+    add_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+
 class Advice(models.Model):
     title = models.CharField(max_length=160)
     content = models.TextField()
     add_date = models.DateTimeField(auto_now_add = True)
     tags = TaggableManager()
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='photos')
     
     def __str__(self):
-        return self.title
+        return self.title   
+
+    
